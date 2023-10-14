ab8f01cb2d98855172eaad3d2f645e3fb261a06b
JP <JPE011221122@gmail.com>
2023-10-13 17:42:04 -0700

    added camera examples for the video

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intermidate/OpenCV_CameraExample.java
@@ -0,0 +1,119 @@
+package org.firstinspires.ftc.teamcode.Intermidate;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@Autonomous(name="OpenCV_CameraExample",group="used")
+//@Disabled
+public class OpenCV_CameraExample extends LinearOpMode {
+
+    private DcMotor motorFL;
+    private DcMotor motorFR;
+    private DcMotor motorBL;
+    private DcMotor motorBR;
+
+
+
+    OpenCvWebcam webcam;
+    OpenCVPipline.OpenCvPipline pipeline;
+    OpenCVPipline.OpenCvPipline.Detection_Positions snapshotAnalysis = OpenCVPipline.OpenCvPipline.Detection_Positions.LEFT; // default
+
+    @Override
+    public void runOpMode() {
+
+        // HardwareMap Section (Used to talk to the driver hub for the configuration)
+
+
+        // Camera
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        pipeline = new OpenCVPipline.OpenCvPipline();
+        webcam.setPipeline(pipeline);
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                // This is in what viewing window the camera is seeing through and it doesn't matter
+                // what orientation it is | UPRIGHT, SIDEWAYS_LEFT, SIDEWAYS_RIGHT, etc.
+
+                webcam.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+            }
+        });
+
+        while (!isStarted() && !isStopRequested()) {
+            telemetry.addData("Realtime analysis", pipeline.getAnalysis());
+            telemetry.update();
+
+            // Don't burn CPU cycles busy-looping in this sample
+            sleep(50);
+        }
+
+        snapshotAnalysis = pipeline.getAnalysis();
+
+
+        telemetry.addData("Snapshot post-START analysis", snapshotAnalysis);
+        telemetry.update();
+
+
+        // Motors
+
+        motorFL = hardwareMap.dcMotor.get("motorFL");
+        motorFR = hardwareMap.dcMotor.get("motorFR");
+        motorBL = hardwareMap.dcMotor.get("motorBL");
+        motorBR = hardwareMap.dcMotor.get("motorBR");
+
+        // Has the Run_Using_Encoders because all the motors use Encoders throughout the code.
+        // You have to have this if you are running encoders.
+
+        motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+        telemetry.addData("Status", "\uD83C\uDD97");
+        telemetry.update();
+        waitForStart();
+
+
+        switch (snapshotAnalysis) {
+            case LEFT: // Level 3
+            {
+
+                break;
+
+            }
+
+
+            case RIGHT: // Level 1
+            {
+
+
+
+                break;
+            }
+
+            case CENTER: // Level 2
+            {
+
+
+                break;
+            }
+
+
+        }
+    }
+}
